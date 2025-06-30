4b2a74d4b9cbc06e211e43260ff282752ccf9941
unknown <ksh03196@stu.woosuk.ac.kr>
2025-07-01 00:12:27 +0900

    feat(validation):passwordSchema 비밀번호 유효성 검사 추가

--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -21,3 +21,8 @@ const handleValidationErrors = (res, errors) => {
 export const intIdSchhema = refine(number(), 'intId', (value) => {
   return Number.isInteger(value) && value > 0 || '유효한 ID 형식이 아닙니다.';
 });
+
+export const passwordSchema = refine(string(), 'password', (value) => {
+  return /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]{8,16}$/.test(value) ||
+    '비밀번호는 영문과 숫자 조합으로 8자에서 16자 사이여야 합니다.';
+});
