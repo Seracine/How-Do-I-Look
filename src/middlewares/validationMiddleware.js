f7c61ff5771195b2528251b97adc23ef93273a4e
unknown <ksh03196@stu.woosuk.ac.kr>
2025-06-30 23:23:28 +0900

    feat(validation):Add handleValidationErrors function

---
+++ src/middlewares/validationMiddleware.js
@@ -0,0 +1,19 @@
+import { array, boolean, defaulted, literal, number, object, optional, refine, string, union } from 'superstruct';
+
+const handleValidationErrors = (res, errors) => {
+
+  const extractedErrors = errors.failures().map(failure => {
+    return {
+      field: failure.path.join('.'),
+      message: failure.message,
+      expected: failure.type,
+      received: typeof failure.value,
+      value: failure.value
+    };
+  });
+  
+  return res.status(422).json({
+    message: '요청 데이터 유효성 검사에 실패했습니다.',
+    errors: extractedErrors,
+  });
+};
