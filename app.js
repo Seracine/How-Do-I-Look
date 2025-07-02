030cfe0683db678449560bd4f8d4f561be2125c8
HsunJ_Local <gkstjswo122@gmail.com>
2025-07-02 15:06:44 +0900

    chore(curation): curation api가 동작하기 위한 코드 수정

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv'
+import curationRouter from './src/routes/curationRoute.js';
+import styleRouter from './src/routes/styleRoute.js';
 
 dotenv.config()
 
@@ -8,5 +10,8 @@ const app = express();
 
 app.use(cors()); //CORS 설정
 app.use(express.json());
+
+app.use('/styles', styleRouter); // 스타일 라우터 설정
+app.use('/curations', curationRouter); // Curation 라우터 설정
 
 app.listen(process.env.PORT || 3000, () => console.log("Server Starting..."));
