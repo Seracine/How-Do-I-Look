c9df6005b342bd98ab19bdef7301cc906f5c1596
HsunJ_Local <gkstjswo122@gmail.com>
2025-07-02 14:43:56 +0900

    feat(curation): curation 라우트 추가

---
+++ src/routes/curationRoute.js
@@ -0,0 +1,20 @@
+import express from 'express';
+// import { validateBody, curatingFormInputSchema } from '../middlewares/validationMiddleware.js';
+import * as curationControllers from '../controllers/curationControllers.js';
+
+const curationRouter = express.Router({ mergeParams: true });
+
+curationRouter.route('/')
+    // Curation 생성
+    .post(curationControllers.createCuration)
+    // .post(validate(curationControllers.createCuration), curationControllers.createCuration)
+    // Curation 목록 조회
+    .get(curationControllers.getCurationList);
+
+curationRouter.route('/:curationId')
+    // Curation 수정
+    .patch(curationControllers.updateCuration)
+    // Curation 삭제
+    .delete(curationControllers.deleteCuration);
+
+export default curationRouter;
