40793aaa86d3bb93ad0da419eb106b0050fa4ca7
HsunJ_Local <gkstjswo122@gmail.com>
2025-07-03 14:17:02 +0900

    fix(curation): 라우터 중첩 위치 변경

--- src/routes/styleRoute.js
+++ src/routes/styleRoute.js
@@ -1,10 +1,6 @@
 import express from 'express';
-import curationRouter from './curationRoute.js'; // Curation 라우터를 임포트하여 중첩
 
 const styleRouter = express.Router();
 
-
-// Curation 라우터를 스타일 라우터에 중첩
-styleRouter.use('/:styleId/curations', curationRouter);
 
 export default styleRouter;
