82272030b46bf546daf0f2cde65c2d6a5f6fb994
Seracine <csyo4025@gmail.com>
2025-07-03 18:34:18 +0900

    feat(style): 스타일 목록 조회 API 추가

--- src/routes/styleRoute.js
+++ src/routes/styleRoute.js
@@ -6,7 +6,9 @@ const styleRouter = express.Router()
 styleRouter.route('/')
     .post(styleController.postStyle)
 //  .post(validations.createStyleValidation, styleController.postStyle)  - 오류 처리 후
+    .get(styleController.getStyleList)
 styleRouter.route('/:styleId')
     .put(styleController.putStyle)
     .delete(styleController.deleteStyle)
+
 export default styleRouter;
