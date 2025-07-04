f67905586bdd4d2178a5140373a88738626f59ea
somang <y1810@naverl.com>
2025-07-04 18:49:58 +0900

    미사용 파일 및 import 제거

--- src/routes/replyRoute.js
+++ src/routes/replyRoute.js
@@ -1,14 +1,10 @@
 import express from 'express';
 import replyController from '../controllers/replyController.js';
-/* import { hashPassword } from '../middlewares/replyHashpassword.js';
-import replyControllers from '../controllers/replyControllers.js'; */
 
 const replyRouter = express.Router();
 
 replyRouter.route('/')
   .post(
-    /*  hashPassword,     // 2. 비밀번호 해시
-     replyControllers          // 3. 컨트롤러 */
     replyController.createReply
   );
   
