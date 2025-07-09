b4914839b8df3706b17a0478a5c56b20fc15283c
Seracine <csyo4025@gmail.com>
2025-07-09 11:46:36 +0900

    refactor(util): convertToEnum.js 리펙토링

--- src/utils/convertToEnum.js
+++ src/utils/convertToEnum.js
@@ -1,63 +1,31 @@
 import { CATEGORY } from '@prisma/client';
 
+const stringToEnumMap = {
+  top: CATEGORY.TOP,
+  bottom: CATEGORY.BOTTOM,
+  outer: CATEGORY.OUTER,
+  dress: CATEGORY.DRESS,
+  shoes: CATEGORY.SHOES,
+  bag: CATEGORY.BAG,
+  accessory: CATEGORY.ACCESSORY,
+};
+
+const enumToStringMap = {
+  [CATEGORY.TOP]: 'top',
+  [CATEGORY.BOTTOM]: 'bottom',
+  [CATEGORY.OUTER]: 'outer',
+  [CATEGORY.DRESS]: 'dress',
+  [CATEGORY.SHOES]: 'shoes',
+  [CATEGORY.BAG]: 'bag',
+  [CATEGORY.ACCESSORY]: 'accessory',
+};
+
 const convertCategoryToEnumType = (str) => {
-    let convertedStr
-    switch (str) {
-        case "top":
-            convertedStr = CATEGORY.TOP;
-            break;
-        case "bottom":
-            convertedStr = CATEGORY.BOTTOM;
-            break;
-        case "outer":
-            convertedStr = CATEGORY.OUTER;
-            break;
-        case "dress":
-            convertedStr = CATEGORY.DRESS;
-            break;
-        case "shoes":
-            convertedStr = CATEGORY.SHOES;
-            break;
-        case "bag":
-            convertedStr = CATEGORY.BAG;
-            break;
-        case "accessory":
-            convertedStr = CATEGORY.ACCESSORY;
-            break;
-        default:
-            convertedStr = null;
-    }
-    return convertedStr;
-}
+  return stringToEnumMap[str] || null;
+};
 
 const convertCategoryToStringType = (arg) => {
-    let convertedEnum
-    switch (arg) {
-        case CATEGORY.TOP:
-            convertedEnum = "top";
-            break;
-        case CATEGORY.BOTTOM:
-            convertedEnum = "bottom";
-            break;
-        case CATEGORY.OUTER:
-            convertedEnum = "outer";
-            break;
-        case CATEGORY.DRESS:
-            convertedEnum = "dress";
-            break;
-        case CATEGORY.SHOES:
-            convertedEnum = "shoes";
-            break;
-        case CATEGORY.BAG:
-            convertedEnum = "bag";
-            break;
-        case CATEGORY.ACCESSORY:
-            convertedEnum = "accessory";
-            break;
-        default:
-            convertedEnum = null;
-    }
-    return convertedEnum;
-}
+  return enumToStringMap[arg] || null;
+};
 
 export { convertCategoryToEnumType, convertCategoryToStringType };
