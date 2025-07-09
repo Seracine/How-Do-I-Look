ddb0080f7637a06b4eeb1c6b1be3afc9a1a0a411
Seracine <csyo4025@gmail.com>
2025-07-09 10:49:24 +0900

    style(tag): 태그 controller, service 클래스로 변경

--- src/services/tagService.js
+++ src/services/tagService.js
@@ -1,7 +1,7 @@
 import { prisma } from '../utils/prismaInstance.js'
 
-const tagService = {
-    getPopularTagList: async () => {
+class TagService{
+    getPopularTagList = async () => {
         const limit = 6; // 최대 몇개까지 가져올건지 figma 기준 
         let popularTagList = await prisma.tag.findMany({
             take: limit, 
@@ -11,7 +11,7 @@ const tagService = {
             orderBy : { count: 'desc' }, // 사용중인 개수 순으로 정렬, count가 높으면 먼저 나오게 됨
         })
         return popularTagList = { tags: popularTagList.map(({ tagname }) => tagname)}; // 프론트에서 tags로 구조분해 하고 있으니 tags에 담아서 리턴
-    },
+    };
 }
 
-export default tagService;
+export default new TagService();
