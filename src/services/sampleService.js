2d2011ef4097c4d76aa03174d887d27b923a4502
Seracine <csyo4025@gmail.com>
2025-07-09 10:59:59 +0900

    style(sample): 샘플 controller, service 클래스로 변경

--- src/services/sampleService.js
+++ src/services/sampleService.js
@@ -1,14 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../utils/prismaInstance.js'
 
-const prisma = new PrismaClient();
-
-const sampleService = {
-    createSample: async (sampleBody) => {
+class SampleService{
+    createSample = async (sampleBody) => {
         // 비즈니스 로직 및 데이터베이스와 상호작용하는 코드를 작성합니다.
         return await prisma.sample.create({
             data: sampleBody
         })
-    },
+    };
 }
 
-export default sampleService;
+export default new SampleService();
