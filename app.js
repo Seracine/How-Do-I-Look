0f7b9771c95224076e71eb8ab7027e22cdbe3c50
unknown <ksh03196@stu.woosuk.ac.kr>
2025-07-10 10:32:59 +0900

    test(app):오리진 옵션 변경

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const uploadsDirPath = path.join(__dirname, 'uploads');
 const corsOptions = {
-  origin: 'https://how-do-i-look-fe-mdss.onrender.com', // 허용할 프론트엔드 도메인 지정
+  origin: 'https://naver.com', // 허용할 프론트엔드 도메인 지정
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메서드
   allowedHeaders: ['Content-Type', 'multipart/form-data'], // 허용할 요청 헤더
   optionsSuccessStatus: 200
